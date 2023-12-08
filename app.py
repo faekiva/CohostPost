e2fea48b02fb0752e5ccbcf8ee5c9797bbdf29b1
Kiva <kivahilgenberg@gmail.com>
2023-12-08 17:20:08 -0500

    added auth

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Request
 from cohost.models.user import User
 from cohost.models.block import MarkdownBlock
+from fastapi.responses import JSONResponse
 import os
 
 
 app = FastAPI()
+
+@app.middleware("http")
+async def authenticate(request: Request, call_next):
+    if request.headers.get("Authorization") != os.getenv("AUTH_TOKEN"):
+        return JSONResponse({"status": "unauthorized"}, status_code=401)
+    return await call_next(request)
 
 @app.post("/")
 async def main(request: Request):
